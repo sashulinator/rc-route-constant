57098a027576ff31b1c5c485ba684686b700a049
savchenko alexander <savchenkoalexander91@gmail.com>
2020-12-20 21:36:33 +0400

    refactoring: add all functions to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Route } from "./route";
 export default Route;
 
+export * from "./getCurrent";
+export * from "./getListByRegExp";
+export * from "./getListForEveryLocation";
 export * from "./setPreviousRoute";
