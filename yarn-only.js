98948ee4ebbb372bc9bbb32e18377b5e7deca09a
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2021-05-19 21:14:38 +0400

    fix: Could not install with npm as a dependency again

--- yarn-only.js
+++ yarn-only.js
@@ -1,13 +1,15 @@
 /**
  * Do NOT allow using `npm` as package manager.
  */
-const packageJSON = require(process.env.INIT_CWD + "/package.json");
-const packageName = packageJSON && packageJSON.name;
+try {
+  const packageJSON = require(process.env.INIT_CWD + "/package.json");
+  const packageName = packageJSON && packageJSON.name;
 
-const isDevelopment = packageName === "@savchenko91/rc-route-constant";
+  const isDevelopment = packageName === "@savchenko91/rc-route-constant";
 
-if (isDevelopment && process.env.npm_execpath.indexOf("yarn") === -1) {
-  console.error("You must use Yarn to install dependencies:");
-  console.error("  $ yarn install");
-  process.exit(1);
-}
+  if (isDevelopment && process.env.npm_execpath.indexOf("yarn") === -1) {
+    console.error("You must use Yarn to install dependencies:");
+    console.error("  $ yarn install");
+    process.exit(1);
+  }
+} catch {}
